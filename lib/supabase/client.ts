f982776283903915ea881020b9f4fffa50d53270
Avijit Dagur <163554942+AvijitDagur12@users.noreply.github.com>
2026-08-17 12:46:20 +0530

    version 1 added

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,11 +3,5 @@ import { createBrowserClient } from '@supabase/ssr'
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      headers: {
-        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`
-      }
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
